bc4e5c8687ecb476b581deff85794c02c5cdb4af
Viniciusog <vinijipa23@gmail.com>
2021-02-22 16:09:53 -0400

    User entity and resource

---
+++ src/main/java/com/viniciusogbr/webservice/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.viniciusogbr.webservice.resources;
+
+import com.viniciusogbr.webservice.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L,
+                "Henrique", "henrique@gmail.com", "69995874596", "senha");
+        return ResponseEntity.ok().body(u);
+    }
+}
