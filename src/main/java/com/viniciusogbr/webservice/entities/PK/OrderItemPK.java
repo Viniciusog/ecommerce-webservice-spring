37fc44acceae740cfe2994680e19b2c0c05d4959
Viniciusog <vinijipa23@gmail.com>
2021-02-23 16:33:55 -0400

    OrderItem, many to many association with extra attributes

---
+++ src/main/java/com/viniciusogbr/webservice/entities/PK/OrderItemPK.java
@@ -0,0 +1,57 @@
+package com.viniciusogbr.webservice.entities.PK;
+
+import com.viniciusogbr.webservice.entities.Order;
+import com.viniciusogbr.webservice.entities.Product;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+    //Funciona como chave estrangeira para a classe (tabela) OrderItem
+
+    private static final Long serialVersionUID = 1L;
+
+    //Temos muitos OrderItemPK (OrderItem) para um Produto
+    //Temos muitos OrderItemPK (OrderItem) para uma Order
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(product, that.product) && Objects.equals(order, that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product, order);
+    }
+}
